2982dcedd96b03dd37418add26d18b293ac25102
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-08-08 19:06:45 -0600

    conexion

---
+++ conexion.php
@@ -0,0 +1,29 @@
+<?php
+$password = "pg2024";
+$username = "postgres";
+$dbname = "postulacion_test";
+$host = "localhost";
+$port = "5432";
+$options = "--client_encoding=UTF8";
+
+try {
+    $connectionDB = "host=$host port=$port dbname=$dbname user=$username password=$password options=$options";
+    $connectionDBsPro = pg_pconnect($connectionDB);
+} catch (\Throwable $e) {
+    echo "Error connecting to data base + ".$e; 
+}
+
+/// Connection to server
+/*
+$password = "sirh2024";
+$username = "postgres";
+$dbname = "sirh";
+$host = "localhost";
+$port = "5432";
+$options = "--client_encoding=UTF8";
+*/
+
+
+
+
+
